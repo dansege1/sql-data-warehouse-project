30cb5cec96d1e3c0fb5e31c9dc20a9e0e77848fb
dansege1 <danielsegun2010@gmail.com>
2026-03-11 17:45:08 +0100

    Create gold.dim_customers view for customer data
    This script creates a view for the Gold layer in the data warehouse, specifically the 'dim_customers' view, which combines data from the silver layer to produce a clean and enriched dataset.

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,47 @@
+/*
+==========================================================================
+DDL Script: Create Gold Views
+
+==========================================================================
+Script Purpose:
+	This script creates view for the Gold layer in the data warehouse.
+	The Gold layer represent the final dimension and fact tables (star schema)
+
+	Each view performs transformations and combines data from silver layer to produce
+	a clean, enriched, and business-ready dataset.
+
+Usage:
+-These views can be queried directly for analytics and reporting.
+==========================================================================
+*/
+--
+==============================================================================
+--Create Dimension: gold.dim_customers
+--
+==============================================================================
+IF OBJECT_ID('gold.dim_customers' 'V') IS NOT NULL
+	DROP VIEW gold.dim_customers;
+
+GO
+
+CREATE OR ALTER VIEW gold.dim_customers AS
+SELECT
+	ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key,--surrogate key
+		ci.cst_id AS customer_id,
+		ci.cst_key AS customer_number,
+		ci.cst_firstname AS first_name,
+		ci.cst_lastname AS last_name,
+		la.cntry AS country,
+		ci.cst_marital_status AS marital_status,
+		CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr --CRM is the Master for gender Info
+			ELSE COALESCE (ca.gen, 'n/a') --DATA INTEGRATION: If CRM gender is not available 
+		END AS gender,	
+		ca.bdate AS birthdate,
+		ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid
+
+GO
